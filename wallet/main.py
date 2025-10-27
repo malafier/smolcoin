b254886a96e9f9397b645f207b937296e2db4eb8
Robert <robert@fast>
2025-10-27 14:59:18 +0100

    added loading and saving from file. small prompt for menu

--- wallet/main.py
+++ wallet/main.py
@@ -1,6 +1,9 @@
 import json
 
-from src.crypto import generate_keys
+from prompt_toolkit import prompt
+
+from src.crypto import decrypt_data, encrypt_data, generate_keys
+from src.storage import load_from_file, save_to_file
 
 ### Wallet State
 keys = set()
@@ -8,7 +11,23 @@ password: str | None = None
 
 
 def main():
+    password = prompt("Enter password: ", is_password=True)
     prv, pub = generate_keys()
+    pair = [{"private_key": prv, "public_key": pub}]
+
+    data = json.dumps(pair)
+    print(data)
+    encrypted = encrypt_data(password, data)
+    print(encrypted)
+    save_to_file("storage.json", [encrypted])
+
+    ldata = load_from_file("storage.json")
+    print(ldata)
+
+    decrypted = decrypt_data(password, ldata[0])
+    print(decrypted)
+
+    print(decrypted == data)
 
 
 if __name__ == "__main__":
--- wallet/src/storage.py
+++ wallet/src/storage.py
@@ -1,6 +1,14 @@
-def load_from_file():
-    pass
+import json
+
+# import os
 
 
-def save_to_file():
-    pass
+def load_from_file(filepath: str) -> list:
+    with open(filepath, "r") as file:
+        data = json.load(file)
+        return data
+
+
+def save_to_file(filepath: str, data: list):
+    with open(filepath, "w") as file:
+        json.dump(data, file, ensure_ascii=False)
