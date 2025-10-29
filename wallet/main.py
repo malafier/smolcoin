c070fd9633fe7acce19e5aa55ffbf4227e78e9cd
Robert Małafiejew <01169593@pw.edu.pl>
2025-10-29 21:21:13 +0100

    qol additions

--- wallet/main.py
+++ wallet/main.py
@@ -46,7 +46,6 @@ def main():
 
     loaded_data = load_from_file(KEY_STORAGE_PATH)
     try:
-        # TODO: dodać # hasła do pliku, tak aby mieć pewność że nie ma ataku na padding
         decrypted = [decrypt_data(password, x) for x in loaded_data]
         for key_pair in decrypted:
             keys.add(tuple(json.loads(key_pair)))
@@ -55,6 +54,8 @@ def main():
         exit(1)
 
     while True:
+        key_options = [(pair, pair[1]) for pair in keys]
+
         radio_options = [
             ("add", "Generate new pair"),
             ("list", "List all pairs"),
@@ -63,8 +64,8 @@ def main():
             ("broadcast", "Brodcast message"),
             ("exit", "Exit"),
         ]
-
-        choice = radiolist_dialog(title="Wallet", values=radio_options).run()
+        text = f"Connected to node: {node_adr}"
+        choice = radiolist_dialog(title="Wallet", text=text, values=radio_options).run()
 
         if choice == "add":
             add_new_keys(password)
@@ -75,12 +76,12 @@ def main():
             message_dialog(title="Keys", text=displayed_text).run()
         elif choice == "delete":
             delete_choice = checkboxlist_dialog(
-                title="Delete keys", values=[(pair, pair[1]) for pair in keys]
+                title="Delete keys", values=key_options
             ).run()
             for pair in delete_choice:
                 delete_key(password, pair)
         elif choice == "connect":
-            node_adr = input_dialog(text="Provide host address").run()
+            node_adr = input_dialog(text="Provide node address").run()
         elif choice == "broadcast":
             if node_adr:
                 broadcast_message(node_adr, "ala ma kota")
