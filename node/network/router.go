d7121d14b3a5f1f3ae853b215002c928c3b8bc40
Robert Małafiejew <01169593@pw.edu.pl>
2025-11-08 20:29:11 +0100

    moved util functions to other file

--- node/network/router.go
+++ node/network/router.go
@@ -14,23 +14,6 @@ import (
 	bc "node/blockchain"
 )
 
-func respondWithJSON(w http.ResponseWriter, code int, payload any) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshaling JSON: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error": "Internal Server Error"}`))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
-
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, map[string]string{"error": message})
-}
-
 type Server struct {
 	State *NodeState
 	Miner *bc.Miner
@@ -188,12 +171,6 @@ func (s *Server) sendMessageToPeer(peer Peer, payload []byte) {
 	defer resp.Body.Close()
 
 	log.Printf("  - [I] Sent to %s (Status: %s)\n", peer.Addr(), resp.Status)
-}
-
-func checkPeerDead(peer Peer) bool {
-	checkClient := http.Client{Timeout: 1 * time.Second}
-	_, err := checkClient.Get(fmt.Sprintf("http://%s/", peer.Addr()))
-	return err != nil
 }
 
 func (s *Server) connectToInitialPeer(initialPeer string) {
---
+++ node/network/utils.go
@@ -0,0 +1,32 @@
+package network
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshaling JSON: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal Server Error"}`))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func checkPeerDead(peer Peer) bool {
+	checkClient := http.Client{Timeout: 1 * time.Second}
+	_, err := checkClient.Get(fmt.Sprintf("http://%s/", peer.Addr()))
+	return err != nil
+}
