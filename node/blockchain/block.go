7bd10d25a241c4aee6934600e00720da3839114e
Robert <robert@fast>
2025-11-17 18:53:28 +0100

    serialization added

--- node/blockchain/block.go
+++ node/blockchain/block.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"structs"
 )
 
 type Block struct {
@@ -34,6 +35,27 @@ func (b *Block) IsValid() bool {
 		return false
 	}
 	return hash(blockJson) == b.Hash
+}
+
+func (b *Block) Serialize() ([]byte, error) {
+	return json.Marshal(b)
+}
+
+func (b *Block) SerializeWithoutHash() ([]byte, error) {
+	data := struct {
+		Index     int    `json:"index"`
+		PrevHash  string `json:"prev_hash"`
+		Timestamp int64  `json:"timestamp"`
+		Data      string `json:"data"`
+		Nonce     int    `json:"nonce"`
+	}{
+		Index:     b.Index,
+		PrevHash:  b.PrevHash,
+		Timestamp: b.Timestamp,
+		Data:      b.Data,
+		Nonce:     b.Nonce,
+	}
+	return json.Marshal(data)
 }
 
 func (b *Block) CreateHash() (string, error) {
