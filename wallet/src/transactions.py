ebbdaa096fa15a849eb2fc4203a942369a29fe2e
Robert Małafiejew <01169593@pw.edu.pl>
2026-01-09 03:08:18 +0100

    typo fix

--- wallet/src/transactions.py
+++ wallet/src/transactions.py
@@ -7,22 +7,22 @@ from src.crypto import sign
 @dataclass
 class Transaction:
     sender: str
-    reciever: str
-    ammount: float
+    receiver: str
+    amount: float
     timestamp: int
     signature: str
 
     def __init__(self) -> None:
         self.sender = ""
-        self.reciever = ""
-        self.ammount = 0.0
+        self.receiver = ""
+        self.amount = 0.0
         self.timestamp = 0
 
     def sign(self, sk: str):
         tx = {
             "sender": self.sender,
-            "reciever": self.reciever,
-            "ammount": int(self.ammount) if self.ammount.is_integer() else self.ammount,
+            "receiver": self.receiver,
+            "amount": int(self.amount) if self.amount.is_integer() else self.amount,
             "timestamp": self.timestamp,
         }
         tx_str = json.dumps(tx, separators=(",", ":"))
