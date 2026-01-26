6d6a5f3714bef290bdad27683c7e6f72bce83fc7
Robert Małafiejew <01169593@pw.edu.pl>
2026-01-26 22:31:07 +0100

    logging to file

--- node/main.go
+++ node/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"time"
@@ -16,15 +18,6 @@ import (
 const DEFAULT_DIFFICULTY int = 5
 
 func main() {
-	// Logging
-	handler := tint.NewHandler(os.Stderr, &tint.Options{
-		Level:      slog.LevelDebug,
-		TimeFormat: time.Kitchen,
-	})
-
-	logger := slog.New(handler)
-	slog.SetDefault(logger)
-
 	// Args
 	port := flag.Int("p", 3000, "The port number for the node to listen on.")
 	host := flag.String("H", "127.0.0.1", "The host address for the node to bind to.")
@@ -32,6 +25,22 @@ func main() {
 	peer := flag.String("P", "", "Address to connect to peer (e.g., 127.0.0.1:3001)")
 	mine := flag.Bool("M", false, "Flag if node is a miner")
 	flag.Parse()
+
+	// Logging
+	file, err := os.OpenFile(fmt.Sprintf("node_%d.log", *port), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open log file: %v\n", err)
+		os.Exit(1)
+	}
+
+	multiWriter := io.MultiWriter(os.Stderr, file)
+	handler := tint.NewHandler(multiWriter, &tint.Options{
+		Level:      slog.LevelDebug,
+		TimeFormat: time.Kitchen,
+	})
+
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
 
 	// Setup
 	var miner *bc.Miner
