03d749602896c7cee696629cbce1e7d4f2bb535a
Robert Małafiejew <01169593@pw.edu.pl>
2026-01-26 23:07:49 +0100

    network transactions simulation finished

--- wallet/simulate_net.py
+++ wallet/simulate_net.py
@@ -6,17 +6,18 @@ from tqdm import tqdm
 
 import src.crypto as crypto
 import src.network as net
-from src.storage import save_to_file
+from src.storage import load_from_file, save_to_file
 from src.transactions import Transaction
 
 TX_URI = "transaction"
 URL_TEMPLATE = "http://localhost:%d/%s"
+ALA_PTH = "ala_store.json"
+BOB_PTH = "bob_store.json"
 
 port = 0
 
 
 def send_tx(sk: str, _from: str, _to: str):
-    time.sleep(1)
     tx = Transaction()
     tx.sender = _from
     tx.receiver = _to
@@ -32,31 +33,43 @@ def register_in_net(pk: str):
 
 def main():
     global port
-    port = int(sys.argv[1])
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else sys.exit(-1)
+    new = sys.argv[2] if len(sys.argv) > 2 else None
     ala = {"login": "ala", "passwd": "ala"}
     bob = {"login": "bob", "passwd": "bob"}
 
-    ala_keys = crypto.generate_keys()
-    bob_keys = crypto.generate_keys()
+    ala_keys = tuple(
+        json.loads(crypto.decrypt_data(ala["passwd"], load_from_file(ALA_PTH)[0]))
+        if new
+        else crypto.generate_keys()
+    )
+    bob_keys = tuple(
+        json.loads(crypto.decrypt_data(bob["passwd"], load_from_file(BOB_PTH)[0]))
+        if new
+        else crypto.generate_keys()
+    )
 
     register_in_net(ala_keys[1])
     register_in_net(bob_keys[1])
 
     for _ in tqdm(range(20)):
         send_tx(ala_keys[0], ala_keys[1], bob_keys[1])
+        time.sleep(1)
     print("ala sent her txs")
 
     for _ in tqdm(range(10)):
         send_tx(bob_keys[0], bob_keys[1], ala_keys[1])
+        time.sleep(1)
     print("bob sent his txs")
 
     ledger = net.get_ledger(f"localhost:{port}", ala_keys[1])
     print("ledger at end:", ledger)
 
-    encrypted_ala = crypto.encrypt_data(ala["passwd"], json.dumps(ala_keys))
-    encrypted_bob = crypto.encrypt_data(bob["passwd"], json.dumps(bob_keys))
-    save_to_file("ala_store.json", [encrypted_ala])
-    save_to_file("bob_store.json", [encrypted_bob])
+    if new is None:
+        encrypted_ala = crypto.encrypt_data(ala["passwd"], json.dumps(ala_keys))
+        encrypted_bob = crypto.encrypt_data(bob["passwd"], json.dumps(bob_keys))
+        save_to_file(ALA_PTH, [encrypted_ala])
+        save_to_file(BOB_PTH, [encrypted_bob])
 
 
 if __name__ == "__main__":
